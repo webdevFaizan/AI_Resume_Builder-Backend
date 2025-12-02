2682a014818a1e2d3385e9f024f556cd9dcf5895
webDevFaizan <webdev.faizan@gmail.com>
2025-12-02 21:46:38 +0530

    Basic backend complete, now ai will be integrated

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -6,9 +6,9 @@ import { upload } from "../configs/multer";
 const resumeRouter = express.Router();
 
 resumeRouter.post('/createResume', protect, createResume);
-resumeRouter.post('/deleteResume', protect, deleteResume);
-resumeRouter.post('/getResumebyId', protect, getResumeById);
-resumeRouter.post('/getPublicResumebyId', getPublicResumeById);
+resumeRouter.delete('/deleteResume/:resumeId', protect, deleteResume);
+resumeRouter.get('/get/:resumeId', protect, getResumeById);
+resumeRouter.get('/public/:resumeId', getPublicResumeById);
 
 // The string 'profilePic' in the upload.single('profilePic') method is the field name that refers to the key under which the uploaded file will be sent in the form request. This can be any valid string that corresponds to the form field name where the file will be uploaded.
 resumeRouter.put('/update', upload.single('image'), protect, updateResume);
