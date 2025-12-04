2320687bdcfc580e4b5e3b6dace5fa559e482621
webDevFaizan <webdev.faizan@gmail.com>
2025-12-04 21:31:04 +0530

    Backend complete first draft

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoutes.js';
 import resumeRouter from './routes/resumeRoutes.js';
+import aiRouter from './routes/aiRoutes.js';
 
 const app = express();
 let PORT = process.env.PORT || 3000;
@@ -15,7 +16,8 @@ app.use(cors());
 
 app.get("/", (req, res) => res.send("Application is live") );
 app.use('/app/users', userRouter);
-app.use('/app/resume', resumeRouter)
+app.use('/app/resume', resumeRouter);
+app.use('/api/ai', aiRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT : ${PORT}`)
